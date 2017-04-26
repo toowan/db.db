a494df94070c1fc903a9789698f60f9dec7599eb
Thuan Tran <mail@toowan.me>
2017-04-25 22:54:27 -0700

    Added contact/about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
 
+  resources :contacts, only: :create
+  get 'about', to: 'contacts#new', as: 'new_contact'
+
 end
